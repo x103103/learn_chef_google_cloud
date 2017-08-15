f007c5fbcc0de61c3520ef2671477468539d79ad
Alexander Oleynik <x103103@gmail.com>
2017-08-15 17:12:49 +0300

    kitchen on google cloud platform

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,6 +15,17 @@ service 'apache2' do
   action [:enable, :start]
 end
 
+group 'web_admin'
+
+user 'web_admin' do
+  group 'web_admin'
+  system true
+  shell '/bin/bash'
+end
+
 template '/var/www/html/index.html' do # ~FC033
   source 'index.html.erb'
+  mode '0644'
+  owner 'web_admin'
+  group 'web_admin'
 end
